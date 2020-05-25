02995de22e0be18d0db0b9011489e2f92980e8da
francesca-campana <franx3rox@gmail.com>
2020-05-25 19:09:25 +0200

    dadi

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
 // Mail Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
-var emailUser = prompt('Qual è la tua email?')
-var emailList = ["ziopaperone@gmail.com", "minnie@gmail.com", "pippo@gmail.com", "topolino@gmail.com", "paperino@gmail.com", "pluto@gmail.com", "gastone@gmail.com"];
 
-emailDaCercare = emailUser;
-emailInLista = false;
+// var emailUser = prompt('Qual è la tua email?')
+// var emailList = ["ziopaperone@gmail.com", "minnie@gmail.com", "pippo@gmail.com", "topolino@gmail.com", "paperino@gmail.com", "pluto@gmail.com", "gastone@gmail.com"];
+//
+// emailDaCercare = emailUser;
+// emailInLista = false;
+//
+// for (var i = 0; i < emailList.length; i++ ){
+//   if (emailUser === emailList[i]) {
+//     emailInLista = true;
+//
+//   }
+// console.log(emailInLista);
+// }
+// if (emailInLista) {
+//   console.log('Lei è in lista');
+// } else {
+//   console.log('Lei non è in lista');
+// }
 
-for (var i = 0; i < emailList.length; i++ ){
-  if (emailUser === emailList[i]) {
-    emailInLista = true;
-
-  }
-console.log(emailInLista);
-}
-if (emailInLista) {
-  console.log('Lei è in lista');
-} else {
-  console.log('Lei non è in lista');
-}
+//Gioco dei dadi
+// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// var playerUno = Math.floor(Math.random() * 6) + 1;
+// var playerDue = Math.floor(Math.random() * 6) + 1;
+// console.log(playerUno);
+// console.log(playerDue);
+// if (playerUno > playerDue) {
+//   console.log('You Win!');
+//
+//
+// } else if (playerUno < playerDue) {
+//   console.log('you Lose!');
+// } else {
+//   console.log('Draw!');
+// }
